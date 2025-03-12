b35b891cd58aab6d1f7520418dc95b0b8130ebb5
dineshwpbot <herathmadusankadinesh@gmail.com>
2025-03-12 21:30:09 +0530

    Update menu3.js

--- plugins/menu3.js
+++ plugins/menu3.js
@@ -2,28 +2,28 @@ const { cmd, commands } = require('../command');
 
 // Voice links (hosted MP3 URLs)
 const voiceLinks = [
-'https://github.com/mrdinesh595/Mssadu/raw/refs/heads/main/database/adobe%201.mp3',
-'https://github.com/mrdinesh595/Mssadu/raw/refs/heads/main/database/adobe%2010.mp3',
-'https://github.com/mrdinesh595/Mssadu/raw/refs/heads/main/database/adobe%2011.mp3',
-'https://github.com/mrdinesh595/Mssadu/raw/refs/heads/main/database/adobe%2012.mp3',
-'https://github.com/mrdinesh595/Mssadu/raw/refs/heads/main/database/adobe%2013.mp3',
-'https://github.com/mrdinesh595/Mssadu/raw/refs/heads/main/database/adobe%2014.mp3',
-'https://github.com/mrdinesh595/Mssadu/raw/refs/heads/main/database/adobe%2015.mp3',
-'https://github.com/mrdinesh595/Mssadu/raw/refs/heads/main/database/adobe%2016.mp3',
-'https://github.com/mrdinesh595/Mssadu/raw/refs/heads/main/database/adobe%202.mp3',
-'https://github.com/mrdinesh595/Mssadu/raw/refs/heads/main/database/adobe%203.mp3',
-'https://github.com/mrdinesh595/Mssadu/raw/refs/heads/main/database/adobe%204.mp3',
-'https://github.com/mrdinesh595/Mssadu/raw/refs/heads/main/database/adobe%205.mp3',
-'https://github.com/mrdinesh595/Mssadu/raw/refs/heads/main/database/adobe%206.mp3',
-'https://github.com/mrdinesh595/Mssadu/raw/refs/heads/main/database/adobe%207.mp3',
-'https://github.com/mrdinesh595/Mssadu/raw/refs/heads/main/database/adobe%208.mp3',
-'https://github.com/mrdinesh595/Mssadu/raw/refs/heads/main/database/adobe%209.mp3'
-];
+'https://files.catbox.moe/4i7ccz.mp3',
+'https://files.catbox.moe/c0rswx.mp3',
+'https://files.catbox.moe/hgyeth.mp3',
+'https://files.catbox.moe/lleedr.mp3'',
+'https://files.catbox.moe/5q1il0.mp3',
+'https://files.catbox.moe/is3w5r.mp3',
+'https://files.catbox.moe/rmb9hb.mp3',
+'https://files.catbox.moe/94f3ah.mp3',
+'https://files.catbox.moe/r59jbk.mp3',
+'https://files.catbox.moe/vazu0l.mp3',
+'https://files.catbox.moe/ed46vg.mp3',
+'https://files.catbox.moe/3arfe3.mp3',
+'https://files.catbox.moe/6pzpfr.mp3',
+'https://files.catbox.moe/dh3724.mp3',
+'https://files.catbox.moe/u6yge7.mp3',
+'https://files.catbox.moe/ciierd.mp3'
+    ];
 
 cmd({
     pattern: "list",
     react: "🛸",
-    alias: ["𝐘𝐨𝐮𝐫 𝐬𝐭𝐚𝐭𝐮𝐬 𝐯𝐞𝐰 𝐁𝐲 𝐐𝐮𝐞𝐞𝐧 𝐬𝐚𝐝𝐮", "list", "commands"],
+    alias: ["Seen Your status Queen sadu", "list", "commands"],
     desc: "Get bot's command list.",
     category: "main",
     use: '.menu3',
